c7fa8374803ccf097d719f0886fb467a97ae38c5
Harry <h@gmail.com>
2024-02-22 23:05:19 -0500

    primer commit

---
+++ FTarjetaCredito/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TarjetaCreditoComponent } from './componentes/tarjeta-credito/tarjeta-credito.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TarjetaCreditoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
