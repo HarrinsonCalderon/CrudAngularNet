114583cddf3cbb975220a1b32084870f9f04372e
Harry <h@gmail.com>
2024-02-29 23:42:18 -0500

    seguir con el update

--- FTarjetaCredito/src/app/componentes/tarjeta-credito/tarjeta-credito.component.ts
+++ FTarjetaCredito/src/app/componentes/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -19,10 +19,10 @@ export class TarjetaCreditoComponent implements OnInit{
   constructor(private fb:FormBuilder){
      this.form=this.fb.group(
       {
-        titular:[''],
-        numeroTarjeta:[''],
-        fechaExpiracion:[''],
-        cvv:[''],
+        titular:['',Validators.required],
+        numeroTarjeta:['',[Validators.required,Validators.maxLength(16),Validators.minLength(16)]],
+        fechaExpiracion:['',[Validators.required,Validators.maxLength(5),Validators.minLength(5)]],
+        cvv:['',[Validators.required,Validators.maxLength(3),Validators.minLength(3)]],
 
       }
      )
@@ -43,5 +43,10 @@ export class TarjetaCreditoComponent implements OnInit{
     this.listTarjetas.push(tarjeta)
     this.form.reset()
     console.log(tarjeta)
+  }
+
+  eliminarTarjeta(index:number){
+    console.log(index)
+    this.listTarjetas.splice(index,1)
   }
 }
